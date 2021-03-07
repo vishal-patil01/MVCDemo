9b2778a14275b40257a169d32849ce32f69773d1
vishalpatil01@hotmail.com <vishalpatil01@hotmail.com>
2021-03-07 14:45:39 +0530

    Added : Custom ModelError

--- BookStore/Controllers/BookStoreController.cs
+++ BookStore/Controllers/BookStoreController.cs
@@ -24,6 +24,7 @@ namespace BookStore.Controllers
             {
 
             }
+            ModelState.AddModelError("","Please fill form");
             return View();
         }
         public IActionResult GetAllBook()
