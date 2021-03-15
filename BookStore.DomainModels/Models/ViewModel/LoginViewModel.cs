f64fb4772955bbe0afc19d7f15b2b1ddae2503be
vishalpatil01@hotmail.com <vishalpatil01@hotmail.com>
2021-03-15 19:50:52 +0530

    Added : Validations For Login & Signup

--- BookStore.DomainModels/Models/ViewModel/LoginViewModel.cs
+++ BookStore.DomainModels/Models/ViewModel/LoginViewModel.cs
@@ -8,9 +8,11 @@ namespace BookStore.DomainModels.Models.ViewModel
     public class LoginViewModel
     {
         [DataType(DataType.EmailAddress)]
+        [Required]
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
+        [Required]
         public string Password { get; set; }
     }
 }
--- BookStore.DomainModels/Models/ViewModel/SignupViewModel.cs
+++ BookStore.DomainModels/Models/ViewModel/SignupViewModel.cs
@@ -8,15 +8,19 @@ namespace BookStore.DomainModels.Models.ViewModel
     public class SignupViewModel
     {
         [DataType(DataType.Text)]
+        [Required]
         public string FirstName { get; set; }
 
         [DataType(DataType.Text)]
+        [Required]
         public string LastName { get; set; }
 
         [DataType(DataType.EmailAddress)]
+        [Required]
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
+        [Required]
         public string Password { get; set; }
     }
 }
